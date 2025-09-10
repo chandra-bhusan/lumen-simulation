c6abfaa8a786e7dcc8c1ca2d4548e42bca449ddf
Aashu Rajbhar <aashurajbhar20@gmail.com>
2025-09-10 22:47:31 +0530

    Added Home page component and updated App.jsx

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to the Home Page 🏠</h1>
+      <p>This is your first custom page in React.</p>
+    </div>
+  );
+}
+
+export default Home;
