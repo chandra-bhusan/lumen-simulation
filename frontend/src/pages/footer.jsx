be287b7345ee7929ba28ddfaa97732b382f4a03b
Aashu Rajbhar <aashurajbhar20@gmail.com>
2025-09-10 23:23:18 +0530

    Added Footer component and integrated in App.jsx

---
+++ frontend/src/pages/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", marginTop: "50px", padding: "20px", background: "#f0f0f0" }}>
+      © 2025 Lumen Hackathon - Your Name
+    </footer>
+  );
+}
+
+export default Footer;
